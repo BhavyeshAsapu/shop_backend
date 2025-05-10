33dba36e105af32218d6bec36ce61ccd2a8e3516
BhavyeshAsapu <bhavyeshbhavye4072@gmail.com>
2025-05-11 05:06:28 +0530

    Update SellerController.java
    imported

--- src/main/java/com/klef/fsd/controller/SellerController.java
+++ src/main/java/com/klef/fsd/controller/SellerController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
 
 import com.klef.fsd.model.Seller;
 import com.klef.fsd.service.SellerService;
