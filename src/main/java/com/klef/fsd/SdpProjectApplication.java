a73434d705593eb10db548de408c670217508895
Bhavyesh <163726860+BhavyeshAsapu@users.noreply.github.com>
2025-09-21 23:30:34 +0530

    Update SdpProjectApplication.java

--- src/main/java/com/klef/fsd/SdpProjectApplication.java
+++ src/main/java/com/klef/fsd/SdpProjectApplication.java
@@ -3,6 +3,7 @@ package com.klef.fsd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @ComponentScan(basePackages = "com.klef.fsd")
@@ -12,5 +13,10 @@ public class SdpProjectApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SdpProjectApplication.class, args);
         System.out.println("LL-CART Backend is Running Successfully ... !!");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SdpProjectApplication.class);
     }
 }
